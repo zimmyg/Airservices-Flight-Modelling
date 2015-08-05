4054a3c6e3d209fcb2a66fe9a0181729565c5788
Tim Gleeson <ta2gleeson@students.latrobe.edu.au>
2015-08-05 22:53:41 +1000

    Inital Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import gov.nasa.worldwindx.examples.ApplicationTemplate;
+
+
+public class Main extends ApplicationTemplate
+{
+	public Main()
+	{
+		ApplicationTemplate.start("TestMain", AppFrame.class);
+	}
+	
+	public static void main(String[] args)
+	{
+		new Main();
+	}
+}
