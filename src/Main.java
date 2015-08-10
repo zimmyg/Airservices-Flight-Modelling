08d1065b93fed3a74dd096ee9c68ed2451d30176
Tim Gleeson <ta2gleeson@students.latrobe.edu.au>
2015-08-10 22:23:22 +1000

    Minor changes - startup is now focussed on airport.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import gov.nasa.worldwind.Configuration;
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwindx.examples.ApplicationTemplate;
-
 
 public class Main extends ApplicationTemplate
 {
-	public Main()
-	{
-		ApplicationTemplate.start("TestMain", AppFrame.class);
-	}
-	
+	protected static class AppFrame extends ApplicationTemplate.AppFrame
+    {
+        public AppFrame()
+        {
+            super(true, true, true);
+            
+        }
+    }
+
 	public static void main(String[] args)
 	{
-		new Main();
+		Configuration.setValue(AVKey.INITIAL_LATITUDE, -37.67);
+        Configuration.setValue(AVKey.INITIAL_LONGITUDE, 144.84);
+        Configuration.setValue(AVKey.INITIAL_ALTITUDE, 5e4);
+        
+		ApplicationTemplate.start("World Wind Application", AppFrame.class);
 	}
 }
